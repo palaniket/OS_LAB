0917a9331e46807b12667f5e5c53ce7caa79f2bd
palaniket <aniketpal9667@gmail.com>
2024-04-14 22:42:37 +0530

    first commit

---
+++ intro.cpp
@@ -0,0 +1,8 @@
+//To print "Hello World" in Linux, you can use the echo command in the terminal:
+// Create a file named hello.sh and add the following code:
+echo "Hello World"
+// Save the file and give it execute permission
+chmod +x hello.sh
+
+//Run the script
+./hello.sh
